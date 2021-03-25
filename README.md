80a1ef40fcec8d12898b448b7230bcfd973a27ea
Kizito007 <nwakakelechi49@gmail.com>
2021-03-25 22:44:36 +0100

    Started boilerplate

---
+++ README.md
@@ -0,0 +1,10 @@
+# CITY-WIDE-STORES BACKEND
+
+## Description
+### This is the where the stores logic and api's are created for the purpose of trading and delivering online
+
+## Installation
+> Fork the Project
+> Then clone using * git clone "link" on your terminal *
+> npm install
+> start: node app.js or nodemon
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express"),
+    app = express();
+    mongoose = require("mongoose"),
+    cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(cors());
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
+
+//setup routes
+// app.use("/users", require("./routes/user"));
+// app.use("/todos", require("./routes/todo"));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
